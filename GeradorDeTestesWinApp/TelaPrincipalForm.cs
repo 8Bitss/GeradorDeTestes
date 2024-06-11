2992e6986b216eca7e21915feebc7f7949297427
PabloKoerich <pablokoerich@hotmail.com>
2024-06-11 17:35:52 -0300

    Add project files.

---
+++ GeradorDeTestesWinApp/TelaPrincipalForm.cs
@@ -0,0 +1,10 @@
+namespace GeradorDeTestesWinApp
+{
+    public partial class TelaPrincipalForm : Form
+    {
+        public TelaPrincipalForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
