c32d6629d54a4496040078f21b3fe123ac153e51
PabloKoerich <pablokoerich@hotmail.com>
2024-06-12 15:26:38 -0300

    Criado Tabela Disciplina

---
+++ GeradorDeTestesWinApp/ModuloDisciplina/TabelaDisciplinaControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorDeTestesWinApp.ModuloDisciplina
+{
+    public partial class TabelaDisciplinaControl : UserControl
+    {
+        public TabelaDisciplinaControl()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
